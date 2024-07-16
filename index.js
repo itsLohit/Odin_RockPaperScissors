9896cda95f58bf4e6b5bce25fc2725eae95cb754
itsLohit <109834876+itsLohit@users.noreply.github.com>
2024-07-16 10:38:26 +0530

    Created UI Using DOM Manipulation
    Added styles using CSS

--- index.js
+++ index.js
@@ -1,83 +1,139 @@
+// Game Variables
 const rock = "rock";
 const paper = "paper";
 const scissors = "scissors";
-
-
 let humanScore = 0;
 let computerScore = 0;
 
-for(let i=0; i<5; i++){
-    playGame()
-}
+
+
+
+// Display
+const scoreHuman = document.querySelector(".scoreHuman");
+const scoreComputer = document.querySelector(".scoreComputer");
+const resultDisplay = document.querySelector(".result-display");
+scoreHuman.textContent="You:" + " " + humanScore;
+scoreComputer.textContent="Computer:" + " " + computerScore;
+
+
+
+
+
+
+
+
+
+// Buttons
+const btnRock = document.querySelector("#btnRock");
+const btnPaper = document.querySelector("#btnPaper");
+const btnScissors = document.querySelector("#btnScissors");
+const btnReset = document.querySelector("#btnReset");
+
+
+
+
+btnRock.addEventListener("click", ()=>{
+    playRound("rock");
+});
+
+
+
+btnPaper.addEventListener("click", ()=>{
+    playRound("paper");
+});
+
+
+
+btnScissors.addEventListener("click", ()=>{
+    playRound("scissors");
+});
+
+
+btnReset.addEventListener("click", ()=>{
+    humanScore = 0;
+    computerScore = 0;
+    scoreHuman.textContent="You:" + " " + humanScore;
+    scoreComputer.textContent="Computer:" + " " + computerScore;
+    resultDisplay.textContent="Let's start the game. Choose between rock, paper or scissors.";
+
+});
+
+
 
 function getComputerChoice(){
-    let randomNum = Math.random();
-    if ((randomNum>=0) && (randomNum<=0.3))
-        return rock;
-    else if ((randomNum>0.3) && (randomNum<=0.6))
-        return paper;
-    else
-    return scissors;
+    const choice = [rock, paper, scissors];
+    return choice[Math.floor(Math.random()*3)];
 }
 
-function getHumanChoice(){
-    let humanChoice = prompt("Enter your choice: rock/paper/scissors");
-    return humanChoice.toLowerCase();
-}
+    
 
-function playGame(){
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
+    function playRound(humanChoice){
+        if((humanScore<5) && (computerScore<5)){
+            const computerChoice = getComputerChoice();
+            let result = "Invalid Input";
+            if (humanChoice===computerChoice){
+                result = "Tie!";
+            }
+             else if(computerChoice===rock){
+                switch(humanChoice){
+                    case paper:
+                        result = "You win! Paper beats Rock";
+                        humanScore+=1;
+                        break;
+                    case scissors:
+                        result = "You lose! Rock beats Scissors";
+                        computerScore+=1;
+                        break;
+                }
+            }
+            else if(computerChoice===paper){
+                switch(humanChoice){
+                    case scissors:
+                        result = "You win! Scissors beats Paper";
+                        humanScore+=1;
+                        break;
+                    case rock:
+                        result = "You lose! Paper beats Rock";
+                        computerScore+=1;
+                        break;
+                }
+            }
+            else if(computerChoice===scissors){
+                switch(humanChoice){
+                    case rock:
+                        result = "You win! Rock beats Scissors";
+                        humanScore+=1;
+                        break;
+                    case paper:
+                        result = "You lose! Scissors beats Paper";
+                        computerScore+=1;
+                        break;
+                }
+            }
+            resultDisplay.textContent=result;
+            scoreHuman.textContent="You:" + " " + humanScore;
+            scoreComputer.textContent="Computer:" + " " + computerScore;
+            console.log("Human=" + humanChoice);
+            console.log("Computer=" + computerChoice);
+        }
 
-    function playRound(humanChoice, computerChoice){
-        let result = "Invalid Input";
-        if (humanChoice==computerChoice){
-            result = "Tie!";
+        else if(humanScore>computerScore){
+            resultDisplay.textContent="Congrats! The Final Winner is You!";
         }
-        else if(computerChoice==rock){
-            switch(humanChoice){
-                case paper:
-                    result = "You win! Paper beats Rock";
-                    humanScore+=1;
-                    break;
-                case scissors:
-                    result = "You lose! Rock beats Scissors";
-                    computerScore+=1;
-                    break;
-            }
+        else{
+            resultDisplay.textContent="Uff! You Lose the Match!";
         }
-        else if(computerChoice==paper){
-            switch(humanChoice){
-                case scissors:
-                    result = "You win! Scissors beats Paper";
-                    humanScore+=1;
-                    break;
-                case rock:
-                    result = "You lose! Paper beats Rock";
-                    computerScore+=1;
-                    break;
-            }
-        }
-        else if(computerChoice==scissors){
-            switch(humanChoice){
-                case rock:
-                    result = "You win! Rock beats Scissors";
-                    humanScore+=1;
-                    break;
-                case paper:
-                    result = "You lose! Scissors beats Paper";
-                    computerScore+=1;
-                    break;
-            }
-        }
-        return result;
+        
+
+        
     }
 
-    console.log(humanSelection)
-    console.log(computerSelection)
-    console.log(playRound(humanSelection, computerSelection))
-    console.log("Computer="+computerScore+" "+"You="+humanScore)
-}
+    
+
+    
+    
+  
+    
 
 console.log("Final Score"+" "+"Computer="+computerScore+" "+"You="+humanScore)
 
